447a8ccaeb800b113a900d9be11e3bc32cfd13a1
aliyin <aliyinayi@yahoo.com>
2022-09-14 01:44:02 +0700

    sizing

---
+++ _app/immutable/chunks/1-f9eb1baa.js
@@ -0,0 +1,1 @@
+import{default as o}from"../components/error.svelte-d0b74d87.js";export{o as component};
---
+++ _app/immutable/chunks/2-589775de.js
@@ -0,0 +1,1 @@
+import{default as e}from"../components/pages/_page.svelte-492b088e.js";export{e as component};
