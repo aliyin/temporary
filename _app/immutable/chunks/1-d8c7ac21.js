6d6aab724e49be1565bc0fbacaa64ee4b6aaad38
aliyin <aliyinayi@yahoo.com>
2022-09-14 01:44:38 +0700

    sizing

--- _app/immutable/chunks/1-d8c7ac21.js
+++
@@ -1,1 +0,0 @@
-import{default as o}from"../components/error.svelte-aa5bc095.js";export{o as component};
--- _app/immutable/chunks/2-46da5aa1.js
+++
@@ -1,1 +0,0 @@
-import{default as e}from"../components/pages/_page.svelte-6ceef40a.js";export{e as component};
